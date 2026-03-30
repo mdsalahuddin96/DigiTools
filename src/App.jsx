11f5f0909d5ef660cdc51947f87a11010bcda9bb
mdsalahuddin96 <salauddincse96@gmail.com>
2026-03-30 11:39:18 +0600

    project setup and  Navbar design complete with full responsive

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Nabvar from "./components/Nabvar"
+
+
+function App() {
+  
+
+  return (
+    <>
+      <header className="bg-base-100 shadow-sm">
+        <Nabvar></Nabvar>
+      </header>
+    </>
+  )
+}
+
+export default App
