185d0217dc4bcf5e154de2fbfa85f220cf57e4b3
mdsalahuddin96 <salauddincse96@gmail.com>
2026-04-01 16:52:37 +0600

    final commit

--- src/components/carts/Emptycart.jsx
+++ src/components/carts/Emptycart.jsx
@@ -4,7 +4,7 @@ const Emptycart = () => {
     return (
         <div className='flex justify-center items-center flex-col gap-4 p-20'>
             <ShoppingCart color='#627382' size={50}></ShoppingCart>
-            <p className='text-[#627382] text-lg'>Your cart is empty</p>
+            <p className='text-[#627382] md:text-lg'>Your cart is empty</p>
         </div>
     );
 };
